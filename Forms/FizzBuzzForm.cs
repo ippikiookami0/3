61347a1e0116890a00a389debc2b739e44b7e83e
ippikiookami0 <127153029+ippikiookami0@users.noreply.github.com>
2023-03-28 00:23:03 +0200

    Add files via upload

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzzForm
+    {
+        public string Result { get; set; }
+      
+        [Display(Name = "Twoje imie: ")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"), MaxLength(100)]
+        public string Name { get; set; }
+        [Display(Name = "Twój rok urodzenia: ")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"), Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int YearOfBirth { get; set; }
+    }
+}
